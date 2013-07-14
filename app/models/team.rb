3a92c7ad95a628a0e860f64c36ad8e7e76586eb5
Vito Genovese <vito@legitbs.net>
2013-07-14 16:23:14 -0400

    ca json on teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,14 @@
 class Team < ActiveRecord::Base
   before_create :set_uuid
 
+  def as_ca_json
+    {
+      teamname: name,
+      uuid: uuid,
+      certname: certname
+    }
+  end
+
   private
   def set_uuid
     self.uuid = SecureRandom.uuid
