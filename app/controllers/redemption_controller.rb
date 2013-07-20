ea1267e78d1d035121a7a938835580b81d856422
Vito Genovese <vito@legitbs.net>
2013-07-20 13:20:18 -0400

    get json from redemption controller

--- app/controllers/redemption_controller.rb
+++ app/controllers/redemption_controller.rb
@@ -1,6 +1,18 @@
 class RedemptionController < ApplicationController
+  protect_from_forgery except: :create
+
   def create
-    tokens = params[:tokens]
-    redemptions = tokens.map{|t| Redemption.redeem_for current_team, t }
+    tokens = params[:tokens] || []
+    redemptions = tokens.inject({}) do |m, t|
+      r = begin 
+            Redemption.redeem_for(current_team, t).uuid
+          rescue => e
+            "error: #{e.message}"
+          end
+      m[t] = r
+      m
+    end
+
+    render json: redemptions.to_json
   end
 end
