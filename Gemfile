6008a6eb5f6190d67ebf207c7a23969bc0f4aea4
Vito Genovese <vito@legitbs.net>
2013-07-27 12:38:42 -0400

    howto on a separate page

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 
+gem 'high_voltage'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Scorebot::Application.routes.draw do
   get "scoreboard", to: 'scoreboard#index', as: 'scoreboard'
   get "dashboard", to: 'dashboard#index', as: 'dashboard'
+  get "howto", to: "high_voltage/pages#show", id: 'howto', as: 'howto'
 
   post "redeem", to: 'redemption#create', as: 'redemption'
 
