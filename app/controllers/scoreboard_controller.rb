679ff0b30302874354c47454c6f9cd111ac79dc9
Vito Genovese <vito@legitbs.net>
2013-07-27 14:29:12 -0400

    query for scoreboard

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -1,5 +1,5 @@
 class ScoreboardController < ApplicationController
   def index
-  	@teams = Team.all
+    @teams = Team.for_scoreboard
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -17,6 +17,26 @@ class Team < ActiveRecord::Base
     @@legitbs ||= find_by uuid: "deadbeef-84c4-4b55-8cef-d9471caf1f86"
   end
 
+  def self.without_legitbs
+    where('certname != ?', 'legitbs')
+  end
+
+  def self.for_scoreboard
+    q = <<-SQL
+      SELECT t.name, count(f.id) as score
+      FROM teams as t
+      left JOIN flags AS f 
+		ON f.team_id = t.id	
+      WHERE t.certname != 'legitbs'
+		GROUP BY t.name
+		ORDER BY 
+			score desc,
+			t.name asc
+    SQL
+    
+    connection.select_all(q).map(&:symbolize_keys)
+  end
+
   private
   def set_uuid
     self.uuid ||= SecureRandom.uuid
