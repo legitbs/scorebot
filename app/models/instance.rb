b23122a7ea70c2a4f51dab7d6692fc5818f16e53
Vito Genovese <vito@legitbs.net>
2013-07-31 13:04:08 -0700

    service status works

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -12,4 +12,21 @@ class Instance < ActiveRecord::Base
     return if availabilities.where(round_id: round).size != 0
     availability = Availability.check self
   end
+
+  def owned_check
+    @owned_check ||= health_check_rounds.map do |r| 
+      redemptions.where(round: r).first
+    end
+  end
+
+  def uptime_check
+    @uptime_check ||= health_check_rounds.map do |r| 
+      availabilities.where(round: r, status: 0).first
+    end
+  end
+
+  private
+  def health_check_rounds
+    @health_check_rounds ||= Round.limit(3).offset(1).order('created_at desc')
+  end
 end
