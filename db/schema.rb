28fd6d5f53a1679ab481167c07ba052a974265a0
Vito Genovese <vito@legitbs.net>
2013-08-01 12:20:54 -0700

    ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130728180622) do
+ActiveRecord::Schema.define(version: 20130801191345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,12 +83,14 @@ ActiveRecord::Schema.define(version: 20130728180622) do
   create_table "rounds", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "ended_at"
   end
 
   create_table "services", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "enabled"
   end
 
   create_table "teams", force: true do |t|
