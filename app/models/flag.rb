3e05ba2506cc27637a75b2baabd4d0253cd3a808
Vito Genovese <vito@legitbs.net>
2013-08-01 11:45:48 -0700

    flag reallocation

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -3,4 +3,27 @@ class Flag < ActiveRecord::Base
   has_many :captures
 
   TOTAL_FLAGS = 50_000
+
+  def self.reallocate(ending_round)
+    transaction do
+      scoring_teams = Team.
+        joins(:redemptions).
+        where(redemptions: {round: ending_round}).
+        distinct.to_a
+
+      divisor = scoring_teams.length
+      available = Team.legitbs.flags.length
+      
+      bundle_size = (available / divisor).floor
+      remainder = available % divisor
+
+      until scoring_teams.empty?
+        team = scoring_teams.pop
+        Team.legitbs.flags.limit(bundle_size).each do |f|
+          f.team = team
+          f.save
+        end
+      end
+    end
+  end
 end
