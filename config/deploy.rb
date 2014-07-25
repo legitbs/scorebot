987a9b55ed5a2ae72ce44c10d7d712b0a328119b
Vito Genovese <vito@legitbs.net>
2014-07-25 00:28:33 -0400

    redo capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,42 +1,58 @@
-set :application, "scorebot"
-set :repository,  "git@waitingf.org:scorebot.git"
+# config valid only for Capistrano 3.1
+lock '3.2.1'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, 'scorebot'
+set :repo_url, 'git@waitingf.org:scorebot.git'
 
-role :web, "scorebot"                          # Your HTTP server, Apache/etc
-role :app, "scorebot"                          # This may be the same as your `Web` server
-role :db,  "scorebot", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-set :user, 'scorebot'
-set :use_sudo, false
-set :deploy_to, '/home/scorebot/app'
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/home/scorebot/production'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
 
 namespace :deploy do
-  task :bundle, roles: :app do
-    run "cd #{latest_release} && bundle install"
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
   end
 
-  task :assets, roles: :app do
-    run "cd #{latest_release} && RAILS_ENV=production rake assets:precompile"
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
   end
-end
 
-before "deploy:create_symlink", "deploy:bundle"
-before "deploy:create_symlink", "deploy:assets"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
 end
