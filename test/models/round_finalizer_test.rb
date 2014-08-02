0933554949d2dd4c0d5abe1dda5573655ed13a8d
Vito Genovese <vito@legitbs.net>
2014-08-02 10:16:49 -0400

    rap game round finalizer

--- test/models/round_finalizer_test.rb
+++ test/models/round_finalizer_test.rb
@@ -1,10 +1,43 @@
 require 'test_helper'
 
 class RoundFinalizerTest < ActiveSupport::TestCase
-  should 'be creatable with a round and service'
-  should 'redistribute flags from redemptions and failed availabilities'
-  should 'calculate number of flags each team is due'
-  should 'prioritize flags for winning-est teams'
-  should 'record distributions on the round'
-  should 'record captures and penalties'
+  setup do
+    @old_rounds = Token::EXPIRATION.times.map { FactoryGirl.create :round }
+    @token = FactoryGirl.create :token, round: @old_rounds.first
+
+    @redemption = FactoryGirl.create :redemption, token: @token
+    @round = @redemption.round
+    @instance = @token.instance
+    @service = @instance.service
+  end
+
+  should 'be creatable with a round and service' do
+    RoundFinalizer.new @round, @service
+  end
+  
+  context 'with redemptions and failed availabilities' do
+    setup do
+      @owned_team = @instance.team
+      @redeeming_team = @redemption.team
+      @idle_team = FactoryGirl.create :team
+
+      @failed_availability = FactoryGirl.create :down_availability, instance: @instance, round: @round
+
+      @finalizer = RoundFinalizer.new @round, @service
+    end
+
+    should 'identify candidate tokens and failed availabilities' do
+      assert_includes @finalizer.candidates, @token
+      assert_includes @finalizer.candidates, @failed_availability
+    end
+    
+    should 'scramble scoring events consistently' do
+      assert_equal @finalizer.candidates, RoundFinalizer.new(@round, @service).candidates
+    end
+
+    should 'include the seed and sequence in the metadata'
+
+    should 'score events in sequence'
+
+  end
 end
