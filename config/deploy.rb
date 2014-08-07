595eada72d20abee73db4c649cf5ea427cb4c51d
Vito Genovese <vito@legitbs.net>
2014-08-07 14:31:35 -0700

    fix service issues, local deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'scorebot'
-set :repo_url, 'git@waitingf.org:scorebot.git'
+set :repo_url, 'file:///home/scorebot/scorebot-repo.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
