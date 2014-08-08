558cbb74a74c3b7cae961a32cbbbe22926c10f62
Vito Genovese <vito@legitbs.net>
2014-08-07 22:41:49 -0700

    fix redistribution

--- app/models/token_redistributor.rb
+++ app/models/token_redistributor.rb
@@ -18,24 +18,33 @@ class TokenRedistributor
   private
   def process_movements_for_service(svc)
     instances = svc.instances
-    recipients = instances.map(&:redemptions).flatten.map(&:team).uniq
+    recipients = instances.map do |i|
+      i.redemptions.where(round: @round).all
+    end.flatten.map(&:team).uniq
 
-    return if recipients.count
+    @movement_json[svc.id] = record = {  }
+
+    record[:instances] = instances.as_json only: :id
+    record[:recipients] = recipients.as_json only: %i{ id certname }
+
+    return if recipients.count == 0
 
     flags = Team.legitbs.flags.where(service: svc).to_a
     return if flags.count < recipients.count
 
     bounty = flags.count / recipients.count
 
-    record = {  }
+    record[:flags] = flags.as_json only: :id
+    record[:bounty] = bounty
+    record[:movements] = {  }
 
     recipients.each do |r|
       bounty.times do
         candidate = flags.pop
         candidate.update_attribute :team_id, r.id
 
-        record[r.id] ||= []
-        record[r.id] << candidate.id
+        record[:movements][r.id] ||= []
+        record[:movements][r.id] << candidate.id
       end
     end
 
