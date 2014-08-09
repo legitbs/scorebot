5f1ddb85c3b7a1196223469ba0ea8809c1730cb2
Vito Genovese <vito@legitbs.net>
2014-08-09 09:15:47 -0700

    make redemption events fire

--- app/controllers/redemption_controller.rb
+++ app/controllers/redemption_controller.rb
@@ -3,11 +3,14 @@ class RedemptionController < ApplicationController
 
   def create
     tokens = params[:tokens] || []
+    successful = []
     redemptions = tokens.inject({}) do |m, t|
       next m if t.blank?
       r = begin 
             Stats.time "#{current_team.certname}.redeem_for" do
-              Redemption.redeem_for(current_team, t).uuid
+              rr = Redemption.redeem_for(current_team, t)
+              successful << rr
+              rr.uuid
             end
           rescue => e
             Scorebot.log "Redeem #{t} failed for #{current_team.try(:name)}: #{e.message}"
@@ -17,7 +20,7 @@ class RedemptionController < ApplicationController
       m
     end
 
-    redemptions.values.each do |r|
+    successful.each do |r|
       Event.new('redemption', r.as_event_json ).publish! rescue nil
     end
 
