0b4059236775ccdfd3a0c830001d684e53f6ba4d
Vito Genovese <vito@legitbs.net>
2015-06-26 23:28:08 -0500

    fix scoreboard view name

--- db/migrate/20150516180242_scorebaord_materialized_view.rb
+++ db/migrate/20150516180242_scorebaord_materialized_view.rb
@@ -1,8 +1,8 @@
 class ScorebaordMaterializedView < ActiveRecord::Migration
   def up
     Team.connection.execute <<-SQL
-      CREATE MATERIALIZED VIEW scoreboard AS
-      SELECT t.id, t.name, count(f.id) as score
+      CREATE MATERIALIZED VIEW scoreboards AS
+      SELECT t.id as id, t.name as name, count(f.id) as score
       FROM teams as t
       left JOIN flags AS f
                 ON f.team_id = t.id
@@ -15,30 +15,12 @@ class ScorebaordMaterializedView < ActiveRecord::Migration
     SQL
 
     Team.connection.execute <<-SQL
-      CREATE UNIQUE INDEX ON scoreboard (id)
-    SQL
-
-    Team.connection.execute <<-SQL
-      CREATE OR REPLACE FUNCTION scoreboard_refresh_proc() RETURNS trigger AS
-      $$
-      BEGIN
-        REFRESH MATERIALIZED VIEW CONCURRENTLY scoreboard;
-      END;
-      $$
-      LANGUAGE plpgsql
-    SQL
-
-    Team.connection.execute <<-SQL
-      CREATE TRIGGER scoreboard_update_trigger
-        AFTER UPDATE ON flags
-        FOR EACH STATEMENT
-        EXECUTE PROCEDURE scoreboard_refresh_proc();
+      CREATE UNIQUE INDEX ON scoreboards (id)
     SQL
   end
 
   def down
-    Team.connection.execute "DROP TRIGGER IF EXISTS scoreboard_update_trigger ON solutions"
-    Team.connection.execute "DROP FUNCTION IF EXISTS scoreboard_refresh_proc"
-    Team.connection.execute "DROP MATERIALIZED VIEW IF EXISTS scoreboard"
+    Team.connection.execute "DROP TRIGGER IF EXISTS scoreboard_update_trigger ON flags"
+    Team.connection.execute "DROP MATERIALIZED VIEW IF EXISTS scoreboards"
   end
 end
