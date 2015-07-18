debcfdd72cf344ef11a51b276a4e1dd80d44598e
Vito Genovese <vito@legitbs.net>
2015-07-18 11:43:03 -0400

    puma config start

---
+++ config/puma.rb
@@ -0,0 +1,10 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 3)
+thread_count = Integer(ENV['WEB_THREADS'] || 5)
+threads thread_count, thread_count
+port ENV['PORT'] || 3000
+
+preload_app!
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
