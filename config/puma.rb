60701e0d436a17f588a641b908851ed9ac64f2bd
Vito Genovese <vito@legitbs.net>
2015-07-26 14:29:57 -0400

    more puma config

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,8 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 3)
 thread_count = Integer(ENV['WEB_THREADS'] || 5)
 threads thread_count, thread_count
 port ENV['PORT'] || 3000
+pidfile 'tmp/pids/puma.pid'
+activate_control_app 'tcp://0.0.0.0:27420', auth_token: 'obWujyosh'
 
 preload_app!
 
