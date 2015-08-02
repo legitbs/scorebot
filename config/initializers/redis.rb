66a488d0d89e66a1deb60941862e108a57944953
Vito Genovese <vito@legitbs.net>
2015-08-02 12:34:24 -0400

    redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,11 @@
-$redis = Redis.new
+redis_config = case Rails.env
+         when 'production'
+           { host: 'bowmore',
+             port: 6379,
+             password: 'vueshosBevounWiedMontowmuzDoHondafDeyWor'
+           }
+         else
+           {  }
+         end
+
+$redis = Redis.new redis_config
