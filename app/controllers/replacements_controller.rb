c5bf43fbd040d635afcb41a127a77411778b901d
Vito Genovese <vito@legitbs.net>
2017-07-24 13:19:47 -0400

    replacement stuff

--- app/controllers/replacements_controller.rb
+++ app/controllers/replacements_controller.rb
@@ -1,7 +1,24 @@
 class ReplacementsController < ApplicationController
   def index
     @services = Service.enabled.all
-    @teams = Team.all
+    @teams = Team.order(certname: :asc).all
+
+    @filters = {}
+    @filters[:team_id] = params[:team_id].to_i if params[:team_id]
+    @filters[:service_id] = Service.enabled.find(params[:service_id]).id if params[:service_id]
+
+    per_page = 50
+
+    @page = (params[:page] || 1).to_i
+    @skip = (@page - 1) * per_page
+
+    @replacements = Replacement.
+                      where(@filters).
+                      limit(50).
+                      offset(@skip).
+                      order(created_at: :desc).
+                      joins(:team, :service).
+                      all
   end
 
   def show
@@ -10,5 +27,34 @@ class ReplacementsController < ApplicationController
   def new
     @services = Service.enabled.all
     @replacement = current_team.replacements.new
+  end
+
+  def create
+    @service = Service.enabled.find(replacement_params[:service_id])
+    @services = Service.enabled.all
+
+    @replacement = current_team.replacements.
+                     new(file: replacement_params[:file],
+                         service: @service,
+                         round: Round.current)
+    begin
+      if @replacement.save
+        return redirect_to replacement_path(@replacement.id),
+                           notice: "Replaced #{@service.name}"
+      end
+    rescue PG::UniqueViolation => e
+      @replacement.errors.add :base, "already replaced this round"
+    rescue => e
+      @replacement.errors.add :base, "problem dog: #{e.message}"
+    end
+
+    render action: 'new'
+  end
+
+  private
+  def replacement_params
+    params.
+      require(:replacement).
+      permit(:service_id, :file)
   end
 end
--- app/models/replacement.rb
+++ app/models/replacement.rb
@@ -2,4 +2,46 @@ class Replacement < ApplicationRecord
   belongs_to :team
   belongs_to :service
   belongs_to :round
+
+  before_save :process_upload
+
+  attr_accessor :file
+
+  MAX_SIZE = (ENV['MAX_REPLACEMENT_SIZE'] || (2 * 1024 * 1024)).to_i # 2mb
+  DIGEST_BLOCK = 1024
+
+  SERVICE_ROOT = ENV['SERVICE_ROOT'] || Rails.root.join("tmp", "services")
+  ARCHIVE_ROOT = ENV['ARCHIVE_ROOT'] || Rails.root.join("tmp", "archive")
+
+
+  def service_path
+    File.join SERVICE_ROOT, team_id.to_s, service.name, 'service.bin'
+  end
+
+  def archive_path
+    File.join SERVICE_ROOT, "#{team_id}-#{service_id}-#{digest}.bin"
+  end
+
+
+  private
+
+  def process_upload
+    raise 'attach a file' unless file
+    raise 'too fat' if file.size > MAX_SIZE
+
+    self.size = file.size
+
+    hash_fn = Digest::SHA256.new
+    file.rewind
+    until file.eof?
+      hash_fn.update file.read(DIGEST_BLOCK)
+    end
+    self.digest = hash_fn.hexdigest
+
+    FileUtils.mkdir_p archive_path
+    FileUtils.mkdir_p service_path
+
+    FileUtils.cp file.path, archive_path
+    FileUtils.cp file.path, service_path
+  end
 end
