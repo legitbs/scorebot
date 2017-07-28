d0b00c437eb4db71d2ca0915837db278aba3816e
Vito Genovese <vito@legitbs.net>
2017-07-27 21:32:18 -0700

    dump to right location

--- pg_dump.sh
+++ pg_dump.sh
@@ -4,4 +4,4 @@ pg_dump -U postgres \
         -h db \
         -d scorebot_production \
         -Fc \
-        -f $(date +/scorebot/tmp/%Y%m%d%H%M%S.pgdump)
+        -f $(date +/scorebot/tmp/dumps/%Y%m%d%H%M%S.pgdump)
