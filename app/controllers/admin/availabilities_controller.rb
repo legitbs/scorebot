cfa51c5707686aea31e4e4acc4781ed31de2f39a
Vito Genovese <vito@legitbs.net>
2017-07-29 15:16:31 -0700

    pagination for availabilities

--- app/controllers/admin/availabilities_controller.rb
+++ app/controllers/admin/availabilities_controller.rb
@@ -1,6 +1,11 @@
 class Admin::AvailabilitiesController < Admin::BaseController
   def index
-    @availabilities = Availability.order(created_at: :desc).limit(Team.count * Service.count)
+    @oof = (params[:offset] || 0).to_i
+
+    @availabilities = Availability.
+                        order(created_at: :desc).
+                        limit(Team.count * Service.count).
+                        offset(@oof)
   end
 
   def show
